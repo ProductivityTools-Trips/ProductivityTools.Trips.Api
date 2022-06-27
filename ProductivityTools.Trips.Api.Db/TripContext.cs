e92cabee3407beab712abcae19a5a9d5c9a122ef
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-06-27 08:34:17 +0200

    context

---
+++ ProductivityTools.Trips.Api.Db/TripContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ProductivityTools.Trips.Api.Db
+{
+    public class TripContext :DbContext
+    {
+
+        private readonly IConfiguration Configuration; 
+
+        public TripContext(IConfiguration conf)
+        {
+            this.Configuration = conf;
+        }
+        public DbSet<Trip> Trips { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = Configuration["ConnectionString"];
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Trip>().ToTable("Trip", "t").HasKey("TripId");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ ProductivityTools.Trips.Api/Controllers/TripController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductivityTools.Trips.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripController : ControllerBase
+    {
+
+        [HttpGet( "Date")]
+        public string Get()
+        {
+            return DateTime.Now.ToString();
+        }
+
+        [HttpGet("List")]
+        public string List()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
