3df25ddf895244aa736ea0b5c3844b36611a0736
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-07-02 11:53:03 +0200

    trips db

--- ProductivityTools.Trips.Api.DbUp/Program.cs
+++ ProductivityTools.Trips.Api.DbUp/Program.cs
@@ -1,2 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DbUp;
+using Microsoft.Extensions.Configuration;
+using ProductivityTools.MasterConfiguration;
+using System.Reflection;
+
 Console.WriteLine("Hello, World!");
+
+IConfigurationRoot configuration = new ConfigurationBuilder()
+.AddMasterConfiguration(configName: "ProductivityTools.Trips.Api.json", true)
+.Build();
+
+
+var connectionString =
+      args.FirstOrDefault()
+      ?? configuration["ConnectionString"];
+EnsureDatabase.For.SqlDatabase(connectionString);
+
+var upgrader =
+    DeployChanges.To
+        .SqlDatabase(connectionString)
+        .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+        .LogToConsole()
+        .Build();
+
+var result = upgrader.PerformUpgrade();
+
+if (!result.Successful)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(result.Error);
+    Console.ResetColor();
+#if DEBUG
+    Console.ReadLine();
+#endif
+    return -1;
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Success!");
+Console.ResetColor();
+return 0;
---
+++ ProductivityTools.Trips.Api.DbUp/Scripts/001.CreateStructure.sql
@@ -0,0 +1,43 @@
+﻿CREATE SCHEMA t;
+GO;
+
+
+CREATE TABLE [t].[Trip](
+	[TripId] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](100) NULL,
+	[DayCount] [int] NULL,
+	[Description] [VARCHAR](MAX) NULL,
+	[Nights] [int] NULL,
+	[DateStart] [date] NULL,
+	[DateEnd] [date] NULL,
+	[Learnings] [varchar](1000) NULL,
+	Constraint PK_TripId PRIMARY KEY (TripId)
+	)
+
+
+	CREATE TABLE [t].[Category](
+	[CategoryId] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](20) NULL,
+	Constraint PK_CategoryId PRIMARY KEY (CategoryId)
+	)
+
+
+	CREATE TABLE [t].[Currency](
+	[CurrencyId] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](20) NULL,
+	Constraint PK_CurrencyId PRIMARY KEY (CurrencyId)
+	)
+
+	CREATE TABLE [t].[TripCurrency](
+	[TripCurrencyId] [int] IDENTITY(1,1) NOT NULL,
+	[CurrencyId] [int] NOT NULL,
+	[TripId] [int] NOT NULL,
+	[Value] [decimal](5, 3) NOT NULL,
+	CONSTRAINT PK_TripCurrencyId PRIMARY KEY (TripCurrencyId),
+	CONSTRAINT FK_TripCurrency_Trip FOREIGN KEY (TripId) REFERENCES [t].[Trip],
+	CONSTRAINT FK_TripCurrency_Currency FOREIGN KEY (CurrencyId) REFERENCES [t].[Currency]
+	)
+
+
+
+
