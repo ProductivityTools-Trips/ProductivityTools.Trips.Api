a2b4fbb6c02346d8b6d14e160f1f46f692760d6b
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-07-03 14:07:35 +0200

    data migration

---
+++ ProductivityTools.Trips.Api.DbUp/DataMigration/001.Initial.sql
@@ -0,0 +1,12 @@
+﻿INSERT INTO [PTTrips].[t].Category(Name)
+SELECT [Name]
+FROM [Powershell].[mw].[Category]
+
+INSERT INTO [PTTrips].[t].Currency(Name)
+SELECT [Name]
+FROM [Powershell].[mw].[Currency]
+
+INSERT INTO [PTTrips].[t].Trip(
+[Name],[DayCount],[Description],[Nights],[DateStart],[DateEnd],[Learnings])
+SELECT [Name],[DayCount],[Description],[Nights],[DateStart],[DateEnd],[Learnings]
+FROM [Powershell].[mw].[Bag]
