feb0cbd54d8d0166d8145b4ab1dc2dd684b0f184
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-07-04 10:03:13 +0200

    migration data script

--- ProductivityTools.Trips.Api.DbUp/DataMigration/001.Initial.sql
+++ ProductivityTools.Trips.Api.DbUp/DataMigration/001.Initial.sql
@@ -10,3 +10,4 @@ INSERT INTO [PTTrips].[t].Trip(
 [Name],[DayCount],[Description],[Nights],[DateStart],[DateEnd],[Learnings])
 SELECT [Name],[DayCount],[Description],[Nights],[DateStart],[DateEnd],[Learnings]
 FROM [Powershell].[mw].[Bag]
+
---
+++ ProductivityTools.Trips.Api.DbUp/DataMigration/002.Difficult.sql
@@ -0,0 +1,19 @@
+﻿/****** Script for SelectTopNRows command from SSMS  ******/
+with x as
+(
+SELECT  bc.[BagCurrencyID]
+      ,bc.[CurrencyID]
+      ,bc.[BagID]
+      ,bc.[Value]
+	  ,b.Name AS oldBagName
+	  ,c.Name AS oldCurrencyName
+	  ,nc.CurrencyId as newCurrencyId
+	  ,t.TripId as newTripId
+  FROM [Powershell].[mw].[BagCurrency] bc
+  INNER JOIN [Powershell].[mw].Bag b ON BC.BagID=B.BagID
+  INNER JOIN [Powershell].[mw].Currency c ON c.CurrencyID=bc.CurrencyID
+  INNER JOIN [PTTrips].[t].Currency nc ON nc.Name=c.Name COLLATE Polish_CI_AS
+  INNER JOIN [PTTrips].t.Trip	t ON t.Name=b.Name COLLATE Polish_CI_AS
+  )
+  INSERT INTO PTTrips.T.TripCurrency (CurrencyId,TripId,Value)
+  SELECT newCurrencyId, newTripId,value FROM X
