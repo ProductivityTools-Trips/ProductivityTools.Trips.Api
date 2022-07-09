ae006b8721e0304a4138cc69feef5597c7fcc83e
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-07-09 21:15:19 +0200

    adding expense

--- ProductivityTools.Trips.Api.Db/Expense.cs
+++ ProductivityTools.Trips.Api.Db/Expense.cs
@@ -9,8 +9,12 @@ namespace ProductivityTools.Trips.Api.Db
     public class Expense
     {
         public int ExpenseId { get; set; }
-        public int TripId { get; set; }
- 
         public string Name { get; set; }
+        public int TripId { get; set; }
+        public int CurrencyId { get; set; }
+        public int CategoryId { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Value { get; set; }
+        public decimal Discount { get; set; }
     }
 }
