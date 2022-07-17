806b5ea66f26c038055da3adac754b24962a039d
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-07-17 10:49:53 +0200

    journal

---
+++ ProductivityTools.Trips.Api.DbUp/Scripts/006.Journal.sql
@@ -0,0 +1,7 @@
+﻿	CREATE TABLE [t].[Journal](
+	[JournalId] [int] IDENTITY(1,1) NOT NULL,
+	[TripId] [int] NOT NULL,
+	[Notes] VARCHAR(MAX)
+	CONSTRAINT PK_JournalId PRIMARY KEY ([JournalId]),
+	CONSTRAINT FK_Journal_Trip FOREIGN KEY (TripId) REFERENCES [t].[Trip],
+	)
