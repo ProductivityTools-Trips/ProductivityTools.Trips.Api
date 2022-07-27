d06bb48e066502c1d4203786e87efb8587d27c9a
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-07-27 14:11:58 +0200

    day full view

---
+++ ProductivityTools.Trips.Api.DbUp/Scripts/011.DayFullView.sql
@@ -0,0 +1,25 @@
+﻿
+ALTER VIEW [t].[TripFullView] 
+AS
+    with expenses as
+    (
+    SELECT e.tripid, sum(e.ValuePln) as Cost
+	      ,sum(e.ExpensedInPln) as Expensed
+	      FROM  t.ExpenseFullView e
+	      GROUP BY e.TripId
+    )
+
+SELECT t.[TripId]
+      ,t.[Name]
+      ,[DayCount]
+      ,[Description]
+      ,[Nights]
+      ,[DateStart]
+      ,[DateEnd]
+      ,[Learnings]
+	  ,e.Cost
+	  ,e.Expensed
+  FROM [PTTrips].[t].[Trip] t
+  LEFT JOIN expenses e ON t.TripId=e.TripId
+GO
+
