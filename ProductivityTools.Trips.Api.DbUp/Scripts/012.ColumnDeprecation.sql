dc957fc20135f51643f478d1e1bf37ca5229054c
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-08-03 07:22:01 +0200

    columns depretation

---
+++ ProductivityTools.Trips.Api.DbUp/Scripts/012.ColumnDeprecation.sql
@@ -0,0 +1,8 @@
+﻿ALTER TABLE [t].[Expense]
+DROP COLUMN ValueAfterDiscount;
+GO
+
+EXEC sp_rename  '[t].[Expense].[Free]', 'Free_To_Delete', 'COLUMN';
+
+EXEC sp_rename  '[t].[Expense].[Discount]', 'Discount_To_Delete', 'COLUMN';
+
