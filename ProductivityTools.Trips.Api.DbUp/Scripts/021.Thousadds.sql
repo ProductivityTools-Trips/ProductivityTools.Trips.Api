5b152f58e2aec1c15c2bf0109a60fb905fe7c8bf
Pawel Wujczyk <pwujczyk@hotmail.com>
2024-08-14 15:03:02 +0200

    alter table

---
+++ ProductivityTools.Trips.Api.DbUp/Scripts/021.Thousadds.sql
@@ -0,0 +1,13 @@
+﻿	ALTER TABLE [t].[Expense]
+	ALTER COLUMN  Value decimal(7,2)
+
+	ALTER TABLE [t].[Expense]
+	ALTER COLUMN  Expensed decimal(7,2)
+
+	ALTER TABLE [t].[Expense]
+	ALTER COLUMN  FamilyCost decimal(7,2)
+
+
+	ALTER TABLE [t].[Expense]
+	ALTER COLUMN  FriendsDebit decimal(7,2)
+
